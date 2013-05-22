9d4c4b361eb824120d45644068618a401fa5b9e4
Sarah Vessels <cheshire137@gmail.com>
2013-05-21 20:13:47 -0400

    Adding options page

---
+++ options.js
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2013 Sarah Vessels
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+function save_options() {
+  var spotify = $('input[name="spotify"]:checked').val();
+  var status_area = $('#status-message');
+  var options = {spotify: spotify};
+  chrome.storage.sync.set({'youtube2spotify_options': options}, function() {
+    status_area.text('Okay, got it!').fadeIn(function() {
+      setTimeout(function() {
+        status_area.fadeOut();
+      }, 2000);
+    });
+  });
+}
+
+function restore_options() {
+  chrome.storage.sync.get('youtube2spotify_options', function(opts) {
+    opts = opts.youtube2spotify_options || {};
+    if (opts.spotify) {
+      var selector = 'input[name="spotify"][value="' + opts.spotify + '"]';
+      $(selector).attr('checked', 'checked');
+    } else {
+      $('#web_player').attr('checked', 'checked');
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
+
+$('input[name="spotify"]').on('change', save_options);
