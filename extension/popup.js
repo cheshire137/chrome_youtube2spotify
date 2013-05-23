7b26b9056e319a84bd80c20a66c9eaeb8496e63e
Sarah Vessels <cheshire137@gmail.com>
2013-05-22 21:03:04 -0400

    Adding analytics, more screenshots

--- extension/popup.js
+++ extension/popup.js
@@ -15,16 +15,18 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-// var _gaq = _gaq || [];
-// _gaq.push(['_setAccount', 'UA-41000832-1']);
-// _gaq.push(['_trackPageview']);
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-41002289-1']);
+_gaq.push(['_trackPageview']);
 
-// (function() {
-//   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-//   ga.src = 'https://ssl.google-analytics.com/ga.js';
-//   var s = document.getElementsByTagName('script')[0];
-//   s.parentNode.insertBefore(ga, s);
-// })();
+(function() {
+  var ga = document.createElement('script'); 
+  ga.type = 'text/javascript'; 
+  ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(ga, s);
+})();
 
 var youtube2spotify_popup = {
   max_display_artists: 3,
