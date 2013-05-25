946768290a3f61fcf79f2b1ca9feaca17b531b59
Sarah Vessels <cheshire137@gmail.com>
2013-05-25 12:06:05 -0400

    Queuing tracks to store, then one write op

--- extension/data.js
+++ extension/data.js
@@ -1,16 +1,26 @@
 var youtube2spotify_data = {
   youtube_developer_key: 'AI39si6koxPUHowThl0aytdIBp8OXNRYu3g08TSBf8UPjuAggM3OQgjh86jyMHj694gf6Aw9lxskseVhHJCcQ-Smem_GX_7dAQ',
+  tracks_to_store: {},
 
-  store_track_data: function(single_track_data, callback) {
+  store_all_tracks: function(callback) {
+    var tracks_to_store = this.tracks_to_store;
+    var me = this;
     chrome.storage.local.get('youtube2spotify', function(data) {
       data = data.youtube2spotify || {};
       var tracks = data.tracks || {};
-      tracks[single_track_data.id] = single_track_data;
+      for (var track_id in tracks_to_store) {
+        tracks[track_id] = tracks_to_store[track_id];
+        delete me.tracks_to_store[track_id];
+      }
       data.tracks = tracks;
       chrome.storage.local.set({'youtube2spotify': data}, function() {
-        callback(single_track_data);
+        callback();
       });
     });
+  },
+
+  queue_track_data_for_storage: function(single_track_data) {
+    this.tracks_to_store[single_track_data.id] = single_track_data;
   },
 
   get_youtube_title: function(video_id, callback) {
@@ -54,12 +64,14 @@ var youtube2spotify_data = {
     var query_url = youtube2spotify_util.get_spotify_track_search_url(title);
     var me = this;
     $.getJSON(query_url, function(data) {
+      console.log('got Spotify data for title ' + title);
       if (data && data.info && data.info.num_results > 0) {
         me.got_spotify_tracks(data, callback);
       } else {
         callback(false);
       }
     }).error(function() {
+      console.log('got Spotify data for title ' + title);
       callback(false);
     });
   },
@@ -103,7 +115,8 @@ var youtube2spotify_data = {
       var corrected_title = chosen_words.join(' ');
       me.get_spotify_data(corrected_title, function(data) {
         if (data) {
-          me.store_track_data(data, callback);
+          me.queue_track_data_for_storage(data);
+          callback(data);
         } else {
           callback(false);
         }
@@ -137,7 +150,8 @@ var youtube2spotify_data = {
     title = youtube2spotify_util.strip_punctuation(title);
     this.get_spotify_data(title, function(data) {
       if (data) {
-        me.store_track_data(data, callback);
+        me.queue_track_data_for_storage(data);
+        callback(data);
       } else {
         me.spotify_match_attempt2(title, s_choice, callback);
       }
@@ -147,6 +161,7 @@ var youtube2spotify_data = {
   store_spotify_track_for_yt_video: function(vid, s_choice, callback) {
     var me = this;
     this.get_youtube_title(vid, function(title) {
+      console.log('got YouTube title for video ID ' + vid);
       if (title) {
         me.spotify_match_attempt1(title, s_choice, callback);
       } else {
@@ -157,15 +172,22 @@ var youtube2spotify_data = {
 
   store_spotify_tracks_from_reddit_data: function(data, s_choice, callback) {
     var youtube_urls = youtube2spotify_util.get_reddit_youtube_urls(data);
+    console.log('got ' + youtube_urls.length + ' YouTube URLs from Reddit data');
     var num_youtube_links = youtube_urls.length;
+    var me = this;
     var on_data_stored = function(i) {
+      console.log('stored data for YouTube URL #' + (i + 1));
       if (i === num_youtube_links - 1) {
-        callback(s_choice);
+        me.store_all_tracks(function() {
+          console.log('finished storing all Spotify tracks for YouTube videos');
+          callback(s_choice);
+        });
       }
     };
-    var me = this;
     var url_handler = function(i) {
+      console.log('handling YouTube video #' + (i+1));
       var vid = youtube2spotify_util.get_youtube_video_id(youtube_urls[i]);
+      console.log('got YouTube video ID for #' + (i+1));
       me.store_spotify_track_for_yt_video(vid, s_choice, function(data) {
         on_data_stored(i);
       });
@@ -216,6 +238,7 @@ var youtube2spotify_data = {
     var me = this;
     this.get_spotify_choice(function(s_choice) {
       me.get_reddit_api_data(subreddit_path, s_choice, function(data) {
+        console.log('got Reddit API data for ' + subreddit_path);
         me.store_spotify_tracks_from_reddit_data(data, s_choice, callback);
       });
     });
